03b7cee2f092ca866014fcbd30b97000636ad63e
Suleim <kotievsuleym@gmail.com>
2026-06-13 18:15:34 +0300

    lazy components

--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -41,7 +41,7 @@ const createCardAction = async (_prevState, formData) => {
   }
 };
 
-export const AddQuestionPage = () => {
+const AddQuestionPage = () => {
   const [formState, formAction, isPending] = useActionState(createCardAction, { clearForm: true });
 
   return (
@@ -127,3 +127,4 @@ export const AddQuestionPage = () => {
     </>
   );
 };
+export default AddQuestionPage;
